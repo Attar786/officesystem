3f7a8ff011a1ea5e6c4745dd2f151afc6758f90f
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-04 18:54:24 +0500

    html pages create

--- emp_app/views.py
+++ emp_app/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def all_emp(request):
+    return render(request,'all.html')
+
+def add_emp(request):
+    return render(request,'add.html')
+
+def remove_emp(request):
+    return render(request,'remove.html')
+
+def filter_emp(request):
+    return render(request,'filter.html')
+
+
