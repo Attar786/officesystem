6823cfbae3564c44c69d96ace3490fe4c206ec0e
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-05 22:24:17 +0500

    remove page working done

--- emp_app/views.py
+++ emp_app/views.py
@@ -27,15 +27,26 @@ def add_emp(request):
         phone = int(request.POST['phone'])
         newemps = Employee(firstname = firstname, lastname= lastname, salary= salary, bonus= bonus, phone=phone , dept_id= dept, role_id= role, hire_date=datetime.now())
         newemps.save()
-        
         return HttpResponse('employ added successfully')
     elif request.method=="GET":
         return render(request,'add.html')
     else:
         return HttpResponse("oopps!! Employee has not been register")
 
-def remove_emp(request):
-    return render(request,'remove.html')
+def remove_emp(request, emp_id= 0):
+    if emp_id:
+        try:
+            remove = Employee.objects.get(id= emp_id)
+            remove.delete()
+            return HttpResponse("Employee Deleted Successfully")
+        except:
+            return HttpResponse("not deleted")
+        
+    emps = Employee.objects.all()
+    context = {
+        "emps": emps
+    }
+    return render(request,'remove.html', context)
 
 def filter_emp(request):
     return render(request,'filter.html')
