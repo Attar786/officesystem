6ffd49ee483f5d0d4b7a94ec0126ae7042d5e8fc
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-06 20:42:25 +0500

    filter page done

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render ,HttpResponse
 from datetime import datetime
 from .models import Employee, Role, Department
+from django.db.models import Q
 # Create your views here.
-
 def index(request):
     return render(request,'index.html')
 
@@ -49,6 +49,23 @@ def remove_emp(request, emp_id= 0):
     return render(request,'remove.html', context)
 
 def filter_emp(request):
-    return render(request,'filter.html')
-
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        employees = Employee.objects.all()
+        if name:
+            employees = employees.filter(Q(firstname__icontains =  name) | Q(lastname__icontains = name))
+        if dept:
+            employees = employees.filter(dept__name__icontains = dept)
+        if role:
+            employees = employees.filter(role__name__icontains = role)
+        context = {
+            "employees" : employees
+        }
+        return render(request,'all.html',context)
+    elif request.method == "GET":
+        return render(request, "filter.html")
+    else:
+        return HttpResponse("error")
 
